31708f2c9dfcfd7ef57f12f80c9d03c9341e0ff7
Joe McGrath <joe@jmcgrath.co.uk>
2018-04-16 19:23:49 +0100

    Update year.

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -5,7 +5,7 @@ import "./Footer.scss";
 const Footer = props => {
   return (
     <footer className="page-footer">
-      <p>Copyright 2017 Joe McGrath</p>
+      <p>Copyright 2018 Joe McGrath</p>
     </footer>
   );
 }; // Footer Component
