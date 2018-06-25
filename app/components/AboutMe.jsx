571cde78c8a198a6499a7e1c500cd0f8ce5bff85
Joe McGrath <joe@jmcgrath.co.uk>
2018-06-25 18:01:24 +0100

    Update AboutMe.jsx

--- app/components/AboutMe.jsx
+++ app/components/AboutMe.jsx
@@ -6,7 +6,7 @@ const AboutMe = props => {
   return (
     <div id="about-me">
       <h2>Looking for a frontend engineer?</h2>
-      <p>I build everything from rich interactive web apps to small websites - take a look at a few of the projects I've been involved in below. Feel free to <a href="mailto:joe@jmcgrath.co.uk" style={{ color: 'white' }}>send me an email</a> if you want to see more!</p>
+      <p>I build everything from highly scaleable web apps to small websites - take a look at a few of the projects I've been involved in below. Feel free to <a href="mailto:joe@jmcgrath.co.uk" style={{ color: 'white' }}>send me an email</a> if you want to see more!</p>
     </div>
   );
 }; // AboutMe Component
